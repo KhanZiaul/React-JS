634254d1bb50e153db02bcaddff1647431e8824e
KHAN ZIAUL HASAN <khanzia16phy102@gmail.com>
2023-03-27 22:47:12 +0600

    local storage file added

---
+++ 02-local-storage/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+function App() {
+
+  return (
+    <div>
+      <h1 style={{textAlign:'center'}}>Welcome to Local Storage</h1>
+    </div>
+  )
+}
+
+export default App
