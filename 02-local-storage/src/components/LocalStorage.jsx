16d25c40f91d8539aabd92a177aa3212a7c8d9cc
KHAN ZIAUL HASAN <khanzia16phy102@gmail.com>
2023-03-27 23:57:12 +0600

    dynamically data added in local storage

--- 02-local-storage/src/components/LocalStorage.jsx
+++ 02-local-storage/src/components/LocalStorage.jsx
@@ -1,6 +1,27 @@
+// normally data added in local storage
+
+// const LocalStorage = (id) => {
+
+//     localStorage.setItem(id,1);
+// };
+
+// dynamically data added in local storage
+
 const LocalStorage = (id) => {
 
-    localStorage.setItem(id,1);
+    let storedCart = localStorage.getItem(id);
+
+    if(storedCart){
+
+        let newquantity = parseInt(storedCart )+ 1; 
+
+        localStorage.setItem(id,newquantity);  
+    }
+    else{
+
+        localStorage.setItem(id,1);  
+    }
+
 };
 
 export default LocalStorage;
