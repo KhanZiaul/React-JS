090a32ca642b9967e13d33ecc12b3d6ac6ed53ea
KHAN ZIAUL HASAN <khanzia16phy102@gmail.com>
2023-04-03 22:59:22 +0600

    useNavigate from react router added

--- 02-react-router/src/componentrs/About_Us/About.jsx
+++ 02-react-router/src/componentrs/About_Us/About.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const About = () => {
+    const navigate = useNavigate();
     return (
         <div>
             <h2>About Us</h2>
             <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Distinctio magnam autem soluta vitae ut expedita iure maxime voluptatibus tenetur, in quae provident molestias reprehenderit explicabo quasi! Harum natus veniam amet cum velit quae autem repellendus eligendi, facere iure numquam, omnis delectus! Inventore incidunt soluta voluptate earum quam nihil cumque maxime est similique excepturi sit nobis dolore cupiditate rem, dolores nemo modi nisi sed eos laudantium tempora quos a! Delectus voluptatem eveniet eius corrupti ducimus libero repudiandae hic ea fuga sed.</p>
+            <button onClick={() => navigate('/')}>Go to Home-page</button>
         </div>
     );
 };
