5a773f821ccb22711ee8f4af48891a0cee69e135
KHAN ZIAUL HASAN <khanzia16phy102@gmail.com>
2023-04-03 22:09:37 +0600

    router NavLink added

--- 02-react-router/src/componentrs/Navigation/Nav.jsx
+++ 02-react-router/src/componentrs/Navigation/Nav.jsx
@@ -1,12 +1,22 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 import './Nav.css'
 const Nav = () => {
     return (
         <nav className='anchor'>
-            <Link to={'/'}>Home</Link>
-            <Link to={'/contact'}>Contact</Link>
-            <Link to={'/about'}>About</Link>
+
+            {/* Router Normal Link */} 
+
+            {/* <Link className="nav-link" to={'/'}>Home</Link>
+            <Link className="nav-link" to={'/contact'}>Contact</Link>
+            <Link className="nav-link" to={'/about'}>About</Link> */}
+
+              {/* Router Nav Link */} 
+
+            <NavLink className="nav-link" to={'/'}>Home</NavLink>
+            <NavLink className="nav-link" to={'/contact'}>Contact</NavLink>
+            <NavLink className="nav-link" to={'/about'}>About</NavLink>
+            
         </nav>
     );
 };
