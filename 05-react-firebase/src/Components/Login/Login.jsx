d3f11ba67e9064cc1310770450b4fe00aa1e06dc
KHAN ZIAUL HASAN <khanzia16phy102@gmail.com>
2023-04-14 04:57:06 +0600

    signout and signin button done

--- 05-react-firebase/src/Components/Login/Login.jsx
+++ 05-react-firebase/src/Components/Login/Login.jsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { GoogleAuthProvider, getAuth, signInWithPopup } from "firebase/auth";
+import React, { useState } from 'react';
+import { GoogleAuthProvider, getAuth, signInWithPopup, signOut } from "firebase/auth";
 import app from '../../Firebase/firebase.init';
 
 const Login = () => {
@@ -7,25 +7,34 @@ const Login = () => {
     const provider = new GoogleAuthProvider()
     console.log(app)
 
+    const [users,setUser] = useState(null)
+
     const googleHandler = () => {
-        signInWithPopup(auth,provider)
-        .then(result => {
-            const user = result.user;
-            console.log(user)
-        })
-        .catch( error => {
-            console.log('error ', error)
-    })
+        signInWithPopup(auth, provider)
+            .then(result => {
+                const user = result.user;
+                console.log(user)
+                setUser(user)
+            })
+            .catch(error => {
+                console.log('error ', error)
+            })
     }
 
-    const signOutHandler = () =>{
-
-
+    const signOutHandler = () => {
+        signOut(auth)
+        .then(result => {
+            console.log(result)
+            setUser(null)
+        }).catch((error) => {
+            console.log(error)
+        });
     }
     return (
         <div className='flex justify-center gap-10 my-10'>
-            <button className='bg-sky-500 text-white px-5 py-3 rounded-md hover:text-sky-700' onClick={googleHandler}>Log In</button>
-            <button className='bg-sky-500 text-white px-5 py-3 rounded-md hover:text-sky-700' onClick={signOutHandler}>sign out</button>
+           {
+             users ? <button className='bg-sky-500 text-white px-5 py-3 rounded-md hover:text-sky-700' onClick={signOutHandler}>sign out</button> :  <button className='bg-sky-500 text-white px-5 py-3 rounded-md hover:text-sky-700' onClick={googleHandler}>Log In</button>
+           }
         </div>
     );
 };
