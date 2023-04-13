00491000fa273edb78db02ec4d67f87879920faa
KHAN ZIAUL HASAN <khanzia16phy102@gmail.com>
2023-04-14 03:50:43 +0600

    login.js updated

--- 05-react-firebase/src/Components/Login/Login.jsx
+++ 05-react-firebase/src/Components/Login/Login.jsx
@@ -1,13 +1,21 @@
 import React from 'react';
-import { GoogleAuthProvider, getAuth } from "firebase/auth";
+import { GoogleAuthProvider, getAuth, signInWithPopup } from "firebase/auth";
 import app from '../../Firebase/firebase.init';
 
 const Login = () => {
     const auth = getAuth(app)
     const provider = new GoogleAuthProvider()
+    console.log(app)
 
     const googleHandler = () => {
-        console.log('clock')
+        signInWithPopup(auth,provider)
+        .then(result => {
+            const user = result.user;
+            console.log(user)
+        })
+        .catch( error => {
+            console.log('error ', error)
+    })
     }
 
     return (
