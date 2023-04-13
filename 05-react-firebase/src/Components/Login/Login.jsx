9784367db671109fbfd3735385fc1f3feb43b6b7
KHAN ZIAUL HASAN <khanzia16phy102@gmail.com>
2023-04-14 03:27:43 +0600

    firebase.init.js done

--- 05-react-firebase/src/Components/Login/Login.jsx
+++ 05-react-firebase/src/Components/Login/Login.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
+import { GoogleAuthProvider, getAuth } from "firebase/auth";
+import app from '../../Firebase/firebase.init';
 
 const Login = () => {
+    const auth = getAuth(app)
+    const provider = new GoogleAuthProvider()
+
+    const googleHandler = () => {
+        console.log('clock')
+    }
+
     return (
         <div>
-            <button>Log In</button>
+            <button onClick={googleHandler}>Log In</button>
         </div>
     );
 };
