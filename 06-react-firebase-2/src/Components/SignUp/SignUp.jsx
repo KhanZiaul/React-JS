4cc6dcc7ffe6634549a693686e65576d2b3237b9
KHAN ZIAUL HASAN <khanzia16phy102@gmail.com>
2023-04-15 03:42:34 +0600

    added onSubmit handler

--- 06-react-firebase-2/src/Components/SignUp/SignUp.jsx
+++ 06-react-firebase-2/src/Components/SignUp/SignUp.jsx
@@ -1,55 +1,26 @@
 import React from 'react';
 
 const SignUp = () => {
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        const Email = e.target.email.value ;
+        const Password = e.target.password.value ;
+        console.log(Email,Password)
+        e.target.reset()
+    }
+
+
     return (
-        <div>
-            <div className="w-full max-w-md mx-auto">
-                <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
-                    <h2 className="text-2xl font-bold mb-6 text-center">Login</h2>
-                    <div className="mb-4">
-                        <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="email">
-                            Email
-                        </label>
-                        <input
-                            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                            id="email"
-                            type="email"
-                            name='email'
-                            placeholder="Email"
-                        />
-                    </div>
-                    <div className="mb-6">
-                        <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
-                            Password
-                        </label>
-                        <input
-                            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
-                            id="password"
-                            name='password'
-                            type="password"
-                            placeholder="Password"
-                        />
-                    </div>
-                    <div className="flex items-center justify-between">
-                        <button
-                            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-                            type="button"
-                        >
-                            Sign In
-                        </button>
-                        <a
-                            className="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800"
-                            href="#"
-                        >
-                            Forgot Password?
-                        </a>
-                    </div>
-                </form>
-                <p className="text-center text-gray-500 text-xs">
-                    &copy; 2023 My Awesome App. All rights reserved.
-                </p>
-            </div>
-    </div>
+        <>
+        <form className='flex flex-col justify-center items-center my-10 gap-3 w-[25%] mx-auto border-2 p-5 bg-slate-300 rounded-md' onSubmit={handleSubmit}>
+            <h2 className='text-xl font-medium'>Please Login</h2>
+            <input className='border-2 px-4 py-2 block border-red-400' name='email' type="email" placeholder='Your email' />
+            <br />
+            <input className='border-2 px-4 py-2  border-red-400' name='password' type="password" placeholder='Your password' />
+            <button className='px-5 py-3 rounded-lg mt-5 bg-sky-500 text-white'>submit</button>
+        </form>
+        </>
     );
 };
 
