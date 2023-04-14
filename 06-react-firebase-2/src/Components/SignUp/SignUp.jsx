568b42866fe6d1566a8117ba51b5ba4d098fb131
KHAN ZIAUL HASAN <khanzia16phy102@gmail.com>
2023-04-15 04:16:51 +0600

    updated

--- 06-react-firebase-2/src/Components/SignUp/SignUp.jsx
+++ 06-react-firebase-2/src/Components/SignUp/SignUp.jsx
@@ -1,22 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import app from '../Firebase/firebase.config';
+import { Link } from 'react-router-dom';
 
 const SignUp = () => {
 
-    const auth = getAuth();
+    const auth = getAuth(app);
+    const [errors , setErrors] = useState('')
 
     const handleSubmit = e => {
         e.preventDefault();
+        setErrors('')
         const Email = e.target.email.value;
         const Password = e.target.password.value;
         console.log(Email, Password)
-
         createUserWithEmailAndPassword(auth, Email, Password)
             .then((userCredential) => {
                 const user = userCredential.user;
             })
             .catch((error) => {
                 const errorMessage = error.message;
+                setErrors(error.message)
             });
 
         e.target.reset()
@@ -26,11 +30,13 @@ const SignUp = () => {
     return (
         <>
             <form className='flex flex-col justify-center items-center my-10 gap-3 w-[25%] mx-auto border-2 p-5 bg-slate-300 rounded-md' onSubmit={handleSubmit}>
-                <h2 className='text-xl font-medium'>Please Login</h2>
+                <h2 className='text-xl font-medium'>Please signup</h2>
                 <input className='border-2 px-4 py-2 block border-red-400' name='email' type="email" placeholder='Your email' />
                 <br />
                 <input className='border-2 px-4 py-2  border-red-400' name='password' type="password" placeholder='Your password' />
+                <p className='py-3'>{errors}</p>
                 <button className='px-5 py-3 rounded-lg mt-5 bg-sky-500 text-white'>submit</button>
+               <p>already signed ? please<Link className='text-blue-700 ms-2' to='/signin'>sign in</Link></p> 
             </form>
         </>
     );
