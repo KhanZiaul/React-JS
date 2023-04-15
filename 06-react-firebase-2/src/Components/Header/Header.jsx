1473fa82a5fe4f446a2b885c9bda26feff8acc28
KHAN ZIAUL HASAN <khanzia16phy102@gmail.com>
2023-04-15 20:45:14 +0600

    updateProfile  done

--- 06-react-firebase-2/src/Components/Header/Header.jsx
+++ 06-react-firebase-2/src/Components/Header/Header.jsx
@@ -4,8 +4,8 @@ import { Link } from 'react-router-dom';
 const Header = () => {
     return (
         <div className='flex justify-center items-center gap-10 mt-10 text-xl font-bold'>
-            <Link className='bg-sky-500 text-white p-3' to='/signin'>Sign In</Link>
             <Link className='bg-sky-500 text-white p-3' to='/signup'>Sign Up</Link>
+            <Link className='bg-sky-500 text-white p-3' to='/signin'>Sign In</Link>
         </div>
     );
 };
--- 06-react-firebase-2/src/Components/SignUp/SignUp.jsx
+++ 06-react-firebase-2/src/Components/SignUp/SignUp.jsx
@@ -1,45 +1,49 @@
 import React, { useState } from 'react';
-import { getAuth, createUserWithEmailAndPassword, sendEmailVerification } from "firebase/auth";
+import { getAuth, createUserWithEmailAndPassword, sendEmailVerification, updateProfile } from "firebase/auth";
 import app from '../Firebase/firebase.config';
 import { Link } from 'react-router-dom';
 
 const SignUp = () => {
 
     const auth = getAuth(app);
-    const [errors , setErrors] = useState('')
+    const [errors, setErrors] = useState('')
 
     const handleSubmit = e => {
         e.preventDefault();
         const Email = e.target.email.value;
         const Password = e.target.password.value;
+        const Name = e.target.name.value;
         setErrors('')
 
-        if(!/(?=.{6,})/.test(Password)){
+        console.log(Name)
+
+        if (!/(?=.{6,})/.test(Password)) {
             setErrors('Passwords should have 8 characters')
-            return ;
+            return;
         }
-        else if(!/(?=.*[a-z])/.test(Password)){
+        else if (!/(?=.*[a-z])/.test(Password)) {
             setErrors('Passwords should have at least one lower case')
-            return ;
+            return;
         }
-        else if(!/(?=.*[A-Z])/.test(Password)){
+        else if (!/(?=.*[A-Z])/.test(Password)) {
             setErrors('Passwords should have at least one upper case')
-            return ;
+            return;
         }
-        else if(!/(?=.*\d)/.test(Password)){
+        else if (!/(?=.*\d)/.test(Password)) {
             setErrors('Passwords should have one digit')
-            return ;
+            return;
         }
-        else if(!/(?=.*[!@#$%^&*])/.test(Password)){
+        else if (!/(?=.*[!@#$%^&*])/.test(Password)) {
             setErrors('Passwords should have one special character')
-            return ;
+            return;
         }
 
         createUserWithEmailAndPassword(auth, Email, Password)
             .then((userCredential) => {
                 const user = userCredential.user;
-                userVarification(userCredential.user)
                 console.log(user)
+                userVarification(userCredential.user)
+                updateUserProfile(userCredential.user , Name)
             })
             .catch((error) => {
                 const errorMessage = error.message;
@@ -51,22 +55,37 @@ const SignUp = () => {
 
     const userVarification = (user) => {
         sendEmailVerification(user)
-        .then(() => {
-            alert('verify email address')
-          });
-        
+            .then(() => {
+                alert('verify email address')
+            });
+
     }
 
+    const updateUserProfile = (user , name) => {
+
+        updateProfile(user, {
+            displayName:name
+        }).then(() => {
+            console.log(name)
+        }).catch((error) => {
+
+        });
+    }
     return (
         <>
             <form className='flex flex-col justify-center items-center my-10 gap-3 w-[25%] mx-auto border-2 p-5 bg-slate-300 rounded-md' onSubmit={handleSubmit}>
                 <h2 className='text-xl font-medium'>Please Sign up</h2>
+
+                <input className='border-2 px-4 py-2 block border-red-400' name='name' type="text" placeholder='Your name' required />
+                <br />
+
                 <input className='border-2 px-4 py-2 block border-red-400' name='email' type="email" placeholder='Your email' required />
                 <br />
-                <input className='border-2 px-4 py-2  border-red-400' name='password' type="password" placeholder='Your password' required/>
+
+                <input className='border-2 px-4 py-2  border-red-400' name='password' type="password" placeholder='Your password' required />
                 <p className='py-3'>{errors}</p>
                 <button className='px-5 py-3 rounded-lg mt-5 bg-sky-500 text-white'>submit</button>
-               <p>already signed ? please<Link className='text-blue-700 ms-2' to='/signin'>sign in</Link></p> 
+                <p>already signed ? please<Link className='text-blue-700 ms-2' to='/signin'>sign in</Link></p>
             </form>
         </>
     );
