6487f8ffda1848ab625480ae9e72c2102be298d5
KHAN ZIAUL HASAN <khanzia16phy102@gmail.com>
2023-04-16 23:17:41 +0600

    Login form Done

--- 07-practice-authentication/src/components/Header/Header.jsx
+++ 07-practice-authentication/src/components/Header/Header.jsx
@@ -9,9 +9,9 @@ const Header = () => {
                     <Link className="btn btn-primary normal-case text-xl">Login Form</Link>
                 </div>
                     <div className="flex gap-8">
-                        <Link>Profile </Link>
                         <Link to='/register'>Register</Link>
                         <Link to='/login'>Login</Link>
+                        <Link>Profile </Link>
                     </div>
             </div>
         </div>
--- 07-practice-authentication/src/components/Login/Login.jsx
+++ 07-practice-authentication/src/components/Login/Login.jsx
@@ -1,9 +1,41 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Login = () => {
     return (
         <div>
-            <h2>this is login</h2>
+            <div className="hero min-h-screen bg-base-200">
+                <div className="hero-content flex-col">
+                    <div className="text-center">
+                        <h1 className="text-3xl font-semibold">Login</h1>
+                    </div>
+                    <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+                        <div className="card-body">
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Email</span>
+                                </label>
+                                <input type="text" placeholder="email" className="input input-bordered" />
+                            </div>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Password</span>
+                                </label>
+                                <input type="text" placeholder="password" className="input input-bordered" />
+                                <label className="label">
+                                    <Link href="#" className="label-text-alt link link-hover">Forgot password?</Link>
+                                </label>
+                            </div>
+                            <div>
+                                <p><small>Are you new ? </small> <Link to='/register' className='text-blue-600 underline hover:text-blue-800 cursor-pointer'>Register</Link></p>
+                            </div>
+                            <div className="form-control mt-6">
+                                <button className="btn btn-primary">Login</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
