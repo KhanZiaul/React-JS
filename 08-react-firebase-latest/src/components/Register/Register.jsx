866976f5ed1c34447943783d9c647a13608ba555
KHAN ZIAUL HASAN <khanzia16phy102@gmail.com>
2023-04-17 02:18:22 +0600

    Register added

---
+++ 08-react-firebase-latest/src/components/Register/Register.jsx
@@ -0,0 +1,47 @@
+import React, { useRef } from 'react';
+import { Link } from 'react-router-dom';
+
+const Register = () => {
+
+    return (
+        <div>
+            <div className="hero bg-base-200">
+                <div className="hero-content flex-col">
+                    <div className="text-center">
+                        <h1 className="text-3xl font-semibold">Register</h1>
+                    </div>
+                    <form className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+                        <div className="card-body">
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Name</span>
+                                </label>
+                                <input type="text" name='name' placeholder="name" className="input input-bordered" required />
+                            </div>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Email</span>
+                                </label>
+                                <input type="email" name='email' placeholder="email" className="input input-bordered" required />
+                            </div>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Password</span>
+                                </label>
+                                <input type="password" name='password' placeholder="password" className="input input-bordered" required />
+                            </div>
+                            <div>
+                                <p><small>Already have an account ? </small> <Link to='/login' className='text-blue-600 underline hover:text-blue-800 cursor-pointer'>Login</Link></p>
+                            </div>
+                            <div className="form-control mt-6">
+                                <button className="btn btn-primary">Register</button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Register;
