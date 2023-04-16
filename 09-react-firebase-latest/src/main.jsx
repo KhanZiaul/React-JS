c10ae7b1678302b6b5baff1de2afcaf81cc40f28
KHAN ZIAUL HASAN <khanzia16phy102@gmail.com>
2023-04-17 01:24:08 +0600

    react router added

--- 09-react-firebase-latest/src/main.jsx
+++ 09-react-firebase-latest/src/main.jsx
@@ -3,8 +3,25 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App></App>,
+    children:[
+      {
+        
+      }
+    ]
+  }
+]);
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
