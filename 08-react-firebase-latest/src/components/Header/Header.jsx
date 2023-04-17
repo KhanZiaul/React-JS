eca0134384d80010c26673c161d1bad2498c770e
KHAN ZIAUL HASAN <khanzia16phy102@gmail.com>
2023-04-17 12:57:02 +0600

    sig out ang log in button condition added

--- 08-react-firebase-latest/src/components/Header/Header.jsx
+++ 08-react-firebase-latest/src/components/Header/Header.jsx
@@ -4,19 +4,30 @@ import { AppsContext } from '../AppContext/AppContext';
 
 const Header = () => {
 
-    const {loginEmail} = useContext(AppsContext)
+    const { loginEmail } = useContext(AppsContext)
 
     return (
         <div>
             <div className="navbar bg-slate-500 text-white px-10">
                 <div className="flex-1">
                     <Link className="btn btn-primary normal-case text-xl">Login Form</Link>
+
                 </div>
-                    <div className="flex gap-8">
-                        <Link to='/register'>Register</Link>
-                        <Link to='/login'>Login</Link>
-                        <Link>Profile </Link>
+                <div className="flex gap-8">
+                    <Link to='/register'>Register</Link>
+                    <Link to='/login'>Login</Link>
+                    <Link>Profile </Link>
+                    <div>
+                        {loginEmail ?
+                            <div className='flex items-center gap-5'>
+                                <p>{loginEmail.email}</p>
+                                <button className='bg-sky-700 px-4 py-2 rounded-md'>Sign Out</button>
+                            </div>
+                            :
+                            <Link to='/login' className='bg-sky-700 px-4 py-2 rounded-md'>Log in</Link>
+                        }
                     </div>
+                </div>
             </div>
         </div>
     );
