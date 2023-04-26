7e5f666203cea5f33df6f9ab0fabbcfb06afb5fa
KHAN ZIAUL HASAN <khanzia16phy102@gmail.com>
2023-04-26 23:06:13 +0600

    header done

--- 09-react-practice-auth/src/Components/Header/Header.jsx
+++ 09-react-practice-auth/src/Components/Header/Header.jsx
@@ -4,12 +4,14 @@ import { Link } from 'react-router-dom';
 const Header = () => {
     return (
         <div>
-            <div className="navbar bg-base-300">
+            <div className="navbar bg-base-300 px-10">
                 <div className="flex-1">
                     <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
                 </div>
-                <div className="flex-none">
+                <div className="flex gap-5">
                     <Link to='/about'>About Us</Link>
+                    <Link to='/login'>Login</Link>
+                    <Link to='/register'>Register</Link>
                 </div>
             </div>
         </div>
