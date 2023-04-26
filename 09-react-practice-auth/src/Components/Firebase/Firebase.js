4297361bfb238406e4d25c441329239102bbdb60
KHAN ZIAUL HASAN <khanzia16phy102@gmail.com>
2023-04-26 23:37:32 +0600

    added firebase

---
+++ 09-react-practice-auth/src/Components/Firebase/Firebase.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAiIxM72uz_zQBLgdZAlcls5NgwrUyl1dU",
+  authDomain: "latest-practice-17c4e.firebaseapp.com",
+  projectId: "latest-practice-17c4e",
+  storageBucket: "latest-practice-17c4e.appspot.com",
+  messagingSenderId: "365089701453",
+  appId: "1:365089701453:web:2638a4173744b9ed95a604"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app ;
