ff38d18a65343e69e86d1d4c61dabe99e1e8c914
KHAN ZIAUL HASAN <khanzia16phy102@gmail.com>
2023-04-27 11:44:32 +0600

    updated

--- 09-react-practice-auth/src/Components/Header/Header.jsx
+++ 09-react-practice-auth/src/Components/Header/Header.jsx
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
+import { parentAuth } from '../Auth/Auth';
 
 const Header = () => {
+    const {isUser} = useContext(parentAuth)
     return (
         <div>
             <div className="navbar bg-base-300 px-10">
@@ -9,6 +11,15 @@ const Header = () => {
                     <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
                 </div>
                 <div className="flex gap-5">
+                    {
+                        isUser ? 
+                        <div className='flex items-center gap-5'>
+                            <p> {isUser.email }</p>
+                            <button className='px-4 py-2 rounded-xl bg-blue-600 text-white cursor-pointer hover:bg-blue-900'>sign out</button> 
+                        </div>
+                        : 
+                        ''
+                    }
                     <Link to='/about'>About Us</Link>
                     <Link to='/login'>Login</Link>
                     <Link to='/register'>Register</Link>
